d6357589bb6b09b8b7744976a4fbe8ae2709eb0a
Julia Pinheiro Oliveira <42902329+JuliaPinheiro@users.noreply.github.com>
2024-11-26 13:28:01 -0300

    Feat(controller):bsm e objetivos

--- src/main/java/com/example/atividade01/controller/HelloWorldController.java
+++ src/main/java/com/example/atividade01/controller/HelloWorldController.java
@@ -1,13 +1,47 @@
 package com.example.atividade01.controller;
 
+import com.example.atividade01.model.Bsm;
+import com.example.atividade01.model.Objetivos;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
     public class HelloWorldController {
+
+
         @GetMapping("/ola")
         public String helloWorld() {
             return "Hello World";
+        }
+
+
+        @GetMapping("/bsm")
+        public List<Bsm> getBsm(){
+            return Arrays.asList(
+                    new Bsm(1, "Mentalidade de Crescimento"),
+                    new Bsm(2, "Responsabilidade Pessoal"),
+                    new Bsm(3, "Orientação ao Futuro"),
+                    new Bsm(4, "Persistência"),
+                    new Bsm(5, "Trabalho em Equipe"),
+                    new Bsm(6, "Comunicação"),
+                    new Bsm(7, "Proatividade"),
+                    new Bsm(8, "Orientação ao Detalhe")
+            );
+         }
+
+
+        @GetMapping("/objetivos")
+        public List<Objetivos> getObjetivos(){
+        return Arrays.asList(
+                new Objetivos(1, "Aprender Spring"),
+                new Objetivos(2, "Estudar para a entrevista"),
+                new Objetivos(3, "Treinar 3x na semana"),
+                new Objetivos(4, "Estudar arquitetura")
+            );
         }
     }
 
---
+++ src/main/java/com/example/atividade01/model/Bsm.java
@@ -0,0 +1,28 @@
+package com.example.atividade01.model;
+
+public class Bsm {
+
+    private int id;
+    private String nome;
+
+    public Bsm(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
